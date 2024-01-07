43cb2bfb2f33f343bc6a66b8d96ebb19b6a2c6e7
kiwirevol <sukhopara.nikita@yandex.ru>
2024-01-07 16:53:51 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import ru.netology.stats.StatsService;
+
+public class Main {
+    public static void main(String[] args) {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        System.out.println(service.sumOfSales(sales));
+        System.out.println(service.averageSumOfSales(sales));
+        System.out.println(service.numMonthOfMaxSales(sales));
+        System.out.println(service.numMonthOfMinSales(sales));
+        System.out.println(service.countMonthOfSalesBelowAverage(sales));
+        System.out.println(service.countMonthOfSalesAboveAverage(sales));
+
+    }
+}
